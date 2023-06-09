dfa6e0c30a25c3d5c0ea7ea06b6c43560ed89a27
isbapna <110186756+isbapna@users.noreply.github.com>
2023-06-09 06:19:55 -0700

    Add files via upload

---
+++ data.js
@@ -0,0 +1,170 @@
+const categories=[
+    {
+        name:'All',
+        filter:'all',
+    },
+    {
+        name: 'Nature',
+        filter:'nature',
+    },
+    {
+        name:'Birds',
+        filter:'birds',
+    },
+    {
+        name:'Animals',
+        filter:'animals',
+    },
+    {
+        name:'Fruits',
+        filter:'fruits',
+    },
+    ];
+    const images =[
+        {
+            id:'1',
+            url:'../picture/1.jpeg',
+            category:'nature',
+            title:'1 Nature',
+        },
+        {
+            id:'2',
+            url:'../picture/2.jpg',
+            category:'nature',
+            title:'2 nature',
+        },
+        {
+            id:'3',
+            url:'../picture/3.jpg',
+            category:'nature',
+            title:'3 nature',
+        },
+        {
+            id:'4',
+            url:'../picture/4.jpeg',
+            category:'nature',
+            title:'4 nature',       
+        },
+        {
+            id:'5',
+            url:'../picture/5.jpeg',
+            category:'nature',
+            title:'5 nature',
+        },
+        {
+            id:'6',
+            url:'../picture/6.jpg',
+            category:'nature',
+            title:'6 nature',
+        },
+        {
+            id:'7',
+            url:'../picture/7.jpeg',
+            category:'nature',
+            title:'7 nature',
+        },
+        {
+            id:'8',
+            url:'../picture/8.jpg',
+            category:'fruits',
+            title:'8 fruits',
+        },
+        {
+            id:'9',
+            url:'../picture/9.jpg',
+            category:'fruits',
+            title:'9 fruits',
+        },
+        {
+            id:'10',
+            url:'../picture/10.jpeg',
+            category:'fruits',
+            title:'10 fruits',
+        },
+        {
+            id:'11',
+            url:'../picture/11.jpeg',
+            category:'fruits',
+            title:'11 fruits',
+        },
+        {
+            id:'12',
+            url:'../picture/12.jpeg',
+            category:'fruits',
+            title:'12 fruits',
+        },
+        {
+            id:'13',
+            url:'../picture/13.jpg',
+            category:'animals',
+            title:'13 animals',
+        },
+        {
+            id:'14',
+            url:'../picture/14.jpeg',
+            category:'animals',
+            title:'14 animals',
+        },
+        {
+            id:'15',
+            url:'../picture/15.jpeg',
+            category:'animals',
+            title:'15 animals',
+        },
+        {
+            id:'16',
+            url:'../picture/16.jpg',
+            category:'animals',
+            title:'16 animals',
+        },
+        {
+            id:'17',
+            url:'../picture/17.jpeg',
+            category:'animals',
+            title:'17 animals',
+        },
+        {
+            id:'18',
+            url:'../picture/18.jpeg',
+            category:'birds',
+            title:'18 birds',
+        },
+        {
+            id:'19',
+            url:'../picture/19.jpeg',
+            category:'birds',
+            title:'19 birds',
+        },
+        {
+            id:'20',
+            url:'../picture/20.jpeg',
+            category:'birds',
+            title:'20 birds',
+        },
+        {
+            id:'21',
+            url:'../picture/21.jpeg',
+            category:'birds',
+            title:'21 birds',
+        },
+        {
+            id:'22',
+            url:'../picture/22.jpeg',
+            category:'birds',
+            title:'22 birds',
+        },
+        {
+            id:'23',
+            url:'../picture/23.jpeg',
+            category:'birds',
+            title:'23 birds',
+        },
+        {
+            id:'24',
+            url:'../picture/24.jpeg',
+            category:'birds',
+            title:'24 birds',
+        },
+
+
+    ];
